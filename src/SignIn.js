31399d438ed51606f2ea00ba4af27c60933f81f6
Sari Stissi <sestissi16@earlham.edu>
2017-06-16 22:59:06 -0400

    Add sign in page

---
+++ src/SignIn.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './SignIn.css'
+
+class SignIn extends Component {
+    render(){
+        return(
+           console.log('Sign In, man.')
+        )
+    }
+}
+
+export default SignIn
