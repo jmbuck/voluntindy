5a9c7bf205f1faa8b9ea0435c9ee87a59b60f3d2
tomtaoran <tomtaoran@aliyun.com>
2017-06-17 11:45:01 -0400

    Add Skills Needed...
    Hopefully not breaking anything

--- src/Opportunities.js
+++ src/Opportunities.js
@@ -25,14 +25,15 @@ class Opportunities extends Component {
         const form = ev.currentTarget
         const title = form.title.value
         const desc = form.desc.value
+        const skills=form.skills.value
         const location = form.location.value
         const date = form.date.value
         const time = form.time.value
         const contact = form.contact.value
         const id = Date.now()
-        const ownerEmail = this.props.firebase.auth().currentUser.email.replace('.', '');
-        const opp = {title, desc, location, date, time, contact, id, ownerEmail}
+        const opp = {title, desc, location, date, time, contact, id}
         const opportunities = [...this.state.opportunities]
+        console.log(opportunities)
         opportunities.unshift(opp)
         this.setState({opportunities})
                 this.props.firebase.database().ref('opportunities').update({
@@ -47,15 +48,16 @@ class Opportunities extends Component {
                 <form onSubmit={this.add.bind(this)}>
                     <input name="title" type="text" placeholder="Title" />
                     <textarea name="desc" type="text" placeholder="Description" ></textarea>
+                    <input name="skills" type="text" placeholder="Skills Needed" />
                     <input name="location" type="text" placeholder="Location" />
                     <input name="date" type="date" placeholder="Date" />
                     <input name="time" type="text" placeholder="Time" />
                     <input name="contact" type="text" placeholder="Contact Information" />
-                    <button type="submit">Add</button>
-                <ul className="Opportunities no-bullet">
-                    {this.state.opportunities.map((opp, i) => <Opportunity key={i} addCredit={this.props.addCredit} opp={opp}/>)}
-                </ul>
+                    <button className="opportunity-button" type="submit">Add</button>
                 </form>
+                <ul className="Opportunities no-bullet">
+                    {this.state.opportunities.map((opp, i) => <Opportunity key={i} opp={opp}/>)}
+                </ul>
             </div>
         )
     }
