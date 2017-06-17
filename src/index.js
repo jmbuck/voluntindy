78ff7326b70db3d36252dc1b7cbb2f6804ec1c65
Jordan Buckmaster <buckmast@purdue.edu>
2017-06-16 22:42:59 -0400

    Get firebase working with project

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import * as firebase from 'firebase';
 
+var config = {
+      apiKey: "AIzaSyC4TZ8u6OsS_3aoUCk9zxKm-zrQLf8ULPw",
+      authDomain: "voluntindy.firebaseapp.com",
+      databaseURL: "https://voluntindy.firebaseio.com",
+      projectId: "voluntindy",
+      storageBucket: "",
+      messagingSenderId: "30007106289"
+};
+let app = firebase.initializeApp(config);
 
-let database = firebase.database();
-ReactDOM.render(<App database = {database} />, document.getElementById('root'));
+ReactDOM.render(<App firebase={app} />, document.getElementById('root'));
 registerServiceWorker();
