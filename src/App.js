9dd0152ada7c4fe0be87c685f2980ecb25b80c5e
Sari Stissi <sestissi16@earlham.edu>
2017-06-17 01:37:49 -0400

    Fix merge conflict while adding user info

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from './NavBar.js'
-<<<<<<< HEAD
 import UserSiteInfo from './UserSiteInfo'
-=======
-import SignIn from './SignIn'
-import SignUp from './SignUp'
->>>>>>> 227229140f671bdb3a7b1f27bf94b906d68fc1ae
 
 class App extends Component {
   constructor(props) {
@@ -17,16 +12,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-<<<<<<< HEAD
         <div className="App-header">
-          <NavBar />
+          <NavBar firebase={this.props.firebase}/>
         </div>
         <div className="Website-info">
           <UserSiteInfo />
         </div>
-=======
-          <NavBar firebase={this.props.firebase}/> 
->>>>>>> 227229140f671bdb3a7b1f27bf94b906d68fc1ae
+           
       </div>
     );
   }
